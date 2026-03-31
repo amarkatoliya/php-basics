74364b2cff185ba62d159ea1e28748244405ac22
amarkatoliya <amarbharavad994@gmail.com>
2026-03-31 09:57:31 +0530

    minor change in require_once in raw

--- product/list.php
+++ product/list.php
@@ -1,5 +1,6 @@
 <?php
-include_once __DIR__ . "/../product.php";
+include_once __DIR__ . "/product.php";
+
 
 $db = new Database("localhost", "root", "xyz", "");
 $db->connect();
--- raw.php
+++ raw.php
@@ -1,5 +1,6 @@
 <?php
-include "database.php";
+include_once __DIR__ . "/database.php";
+
 
 class Row
 {
